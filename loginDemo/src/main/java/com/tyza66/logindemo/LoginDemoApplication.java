31194b8300ccf501f601ac904c84f3e5e8911380
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-19 16:21:48 +0800

    开始

---
+++ loginDemo/src/main/java/com/tyza66/logindemo/LoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tyza66.logindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDemoApplication.class, args);
+	}
+
+}
