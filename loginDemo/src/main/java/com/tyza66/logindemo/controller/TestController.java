ea06c5250af7a241e0e5a7a50838d87e1603ca91
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-20 09:07:21 +0800

    改一下图

--- loginDemo/src/main/java/com/tyza66/logindemo/controller/TestController.java
+++ loginDemo/src/main/java/com/tyza66/logindemo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.tyza66.logindemo.controller;
 
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaCheckRole;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/t1")
+    @SaCheckPermission("t1")//具有t1的权限才能调用
     public String test1(){
         return "ok";
     }
 
     @GetMapping("/t2")
+    @SaCheckLogin //登陆了才能调用
     public String test2(){
         return "ok";
     }
 
     @GetMapping("/t3")
+    @SaCheckRole("admin") //具有admin角色才能调用
     public String test3(){
         return "ok";
     }
